9414a572c00505d215636e82fffa631026aee8db
sergeyrid0 <sergeyrid@yandex.ru>
2020-07-10 23:42:20 +0300

    characteristics

---
+++ Assets/Scripts/characteristics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public static float speed = 100;
+    public static float jumpHeight = 100;
+    public static float airControl = 100;
+    public static float hp = 100;
+    public static float strength = 100;
+    public static float attackSpeed = 100;
+    public static float dizziness = 100;
+}
