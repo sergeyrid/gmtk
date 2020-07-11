19f3183ecff5bfb1f95f8fa509fbbf204d5b3fb9
serchemach <serchemach@gmail.com>
2020-07-11 17:37:39 +0300

    Potion
    Potion functionality(q)

---
+++ Assets/Scripts/PotionControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionControl : MonoBehaviour
+{
+    Characteristics stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = GetComponent<Characteristics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            stats.speed += 30;
+            stats.acceleration += 10;
+            stats.slipperiness += 0.95f;
+            stats.jumpHeight += 2000;
+            stats.airControl += 0.3f;
+            stats.hp += 10;
+            stats.strength += 20;
+            stats.dizziness += 10;
+            stats.attackReach += 50;
+        }
+    }
+}
