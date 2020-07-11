007b362746a005d837a0c2f93a229f65761a1321
uzer037 <dolgishev02@gmail.com>
2020-07-11 12:22:27 +0300

    CameraFollow

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool doFollow;
+    public float speed = 1;
+    public GameObject objToTrack;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(doFollow)
+        {
+            Vector3 pos = Vector2.Lerp(transform.position, objToTrack.transform.position, Time.deltaTime/speed);
+            pos.z = -10;
+            transform.position = pos;
+        }
+    }
+}
