f7fc89cb7b6f62261cb381864ac52bcebce3671f
serchemach <serchemach@gmail.com>
2020-07-12 00:21:24 +0300

    Agani
    in

--- Assets/Scripts/EvilAi.cs
+++ Assets/Scripts/EvilAi.cs
@@ -10,14 +10,20 @@ public class EvilAi : MonoBehaviour
     public int playerLayer = 2;
     public float attackReach;
     public float damage;
+    public float losedistance;
+    public float sightdistance;
 
     Rigidbody2D body;
 
     int direction = -1;
     int state = 0;
+    bool sightedenemy = false;
+    bool attacking = false;
+    float attacktimestart;
     float previoustime;
     Animator anim;
     GameObject eye;
+    GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -91,13 +97,64 @@ public class EvilAi : MonoBehaviour
         {
             Controls cont = player.transform.gameObject.GetComponent<Controls>();
             cont.TakeDamage(damage);
+            Debug.Log("TookDamage");
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        enemy = other.gameObject;
+        sightedenemy = true;
+        state = 0;
+        Debug.Log("Collided");
     }
 
     // Update is called once per frame
     void Update()
     {
-        dowalkcycle();
+        if (attacking)
+        {
+            Debug.Log("JUST A PUSSY");
+            float delta = Time.time - attacktimestart;
+            if (delta <= 0.32 && delta >= 0.28)
+            {
+                Attack();
+                attacking = false;
+                anim.SetTrigger("ChangeState");
+            }
+        }
+        else if (sightedenemy)
+        {
+            Debug.Log("FUCKING PUSSY");
+            float dist = Vector3.Distance(enemy.transform.position, transform.position);
+            if ((enemy.transform.position.x - transform.position.x) * direction < 0)
+            {
+                direction *= -1;
+                flip();
+            }
+
+            if (dist > losedistance)
+            {
+                sightedenemy = false;
+                previoustime = Time.time;
+                direction = -1;
+            }
+            else if (dist > attackReach) 
+            {
+                body.velocity = new Vector2(speed * direction, body.velocity.y);
+            }
+            else
+            {
+                attacking = true;
+                anim.SetTrigger("Attack");
+                attacktimestart = Time.time;
+            }
+        }
+        else
+        {
+            Debug.Log("WALKING MENACE");
+            dowalkcycle();
+        }
         //Debug.Log(previoustime.ToString()+' '+ Time.time.ToString());
     }
 }
