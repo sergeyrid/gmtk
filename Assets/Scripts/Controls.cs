3d25de5be922c60be4f4c494b0232faf87d7133c
sergeyrid0 <sergeyrid@yandex.ru>
2020-07-11 23:34:31 +0300

    almost normal flip

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -34,6 +34,8 @@ public class Controls : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         height = GetComponent<Collider2D>().bounds.size.y;
         width = GetComponent<Collider2D>().bounds.size.x;
+        Debug.Log(width);
+        Debug.Log(height);
     }
 
     void Update() {
@@ -47,13 +49,47 @@ public class Controls : MonoBehaviour
         dizziness = stats.dizziness;
         attackReach = stats.attackReach;
         attackCooldown = stats.attackCooldown;
-        if (body.velocity.x > 0)
+        if (body.velocity.x > 0 && direction == -1)
         {
+            Vector3 pos = transform.position;
+            transform.position = new Vector3(pos.x, pos.y, -pos.z);
             direction = 1;
+            transform.Rotate(0, 180, 0);
+            Transform[] allChildren = GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildren)
+            {
+                Vector3 p = child.localPosition;
+                child.localPosition = new Vector3(p.x, p.y, -p.z);
+            }
+            HingeJoint2D[] allChildrenJoint = GetComponentsInChildren<HingeJoint2D>();
+            foreach (HingeJoint2D child in allChildrenJoint)
+            {
+                Vector3 anc = child.anchor;
+                child.anchor = new Vector2(-anc.x, anc.y);
+                anc = child.connectedAnchor;
+                child.connectedAnchor = new Vector2(-anc.x, anc.y);
+            }
         }
-        else if (body.velocity.x < 0)
+        else if (body.velocity.x < 0 && direction == 1)
         {
+            Vector3 pos = transform.position;
+            transform.position = new Vector3(pos.x, pos.y, -pos.z);
             direction = -1;
+            transform.Rotate(0, 180, 0);
+            Transform[] allChildrenTransform = GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildrenTransform)
+            {
+                Vector3 p = child.localPosition;
+                child.localPosition = new Vector3(p.x, p.y, -p.z);
+            }
+            HingeJoint2D[] allChildrenJoint = GetComponentsInChildren<HingeJoint2D>();
+            foreach (HingeJoint2D child in allChildrenJoint)
+            {
+                Vector3 anc = child.anchor;
+                child.anchor = new Vector2(-anc.x, anc.y);
+                anc = child.connectedAnchor;
+                child.connectedAnchor = new Vector2(-anc.x, anc.y);
+            }
         }
         ProcessInput();
         if (hp == 0)
@@ -95,12 +131,15 @@ public class Controls : MonoBehaviour
         Vector3 pos = transform.position;
         RaycastHit2D hit = Physics2D.BoxCast(new Vector2(pos.x, pos.y - height / 2),
                                              new Vector2(width / 2, delta), 0, -Vector2.up, ~(1<<2));
+        Debug.Log(pos);
         if (hit.collider != null)
         {
+            // Debug.Log("Nice");
             onGround = true;
         }
         else
         {
+            // Debug.Log(":(");
             onGround = false;
         }
     }
