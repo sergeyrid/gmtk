a386e0240a3ff4992c35c535ce9e2ea30866c691
sergeyrid0 <sergeyrid@yandex.ru>
2020-07-12 14:44:01 +0300

    potions fixed

---
+++ Assets/Scripts/GetPotions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetPotions : MonoBehaviour
+{
+    public GameObject player;
+    Characteristics stats;
+    Text textbox;
+
+    void Start()
+    {
+        stats = player.GetComponent<Characteristics>();
+        textbox = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        textbox.text = stats.potions.ToString();
+    }
+}
